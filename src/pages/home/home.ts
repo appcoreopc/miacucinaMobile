ac1eb4a3daf3cfb490673d3f60eff7b52f4ccb13
jeremy_woo <jeremy.woo@clearpoint.co.nz>
2017-09-25 14:14:02 +1300

    gettig input from select.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {TourService} from '../../service/tourService';
-import { NavController, NavParams, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -9,19 +9,32 @@ import { NavController, NavParams, LoadingController } from 'ionic-angular';
 })
 export class HomePage {
   data : any; 
-
+  
   countryData : any; 
   cityData : any;
 
   constructor(public navCtrl: NavController, 
-    private service: TourService, private loader: LoadingController) {
+    private service: TourService, private loader: LoadingController, private alertCtrl: AlertController) {
 
     let loadingUI = this.loader.create({ content: 'Starting tour ...' });
     //loadingUI.present();
 
-    this.service.getFeed().then(data => {
-      this.data = data.feed;
+    //this.service.getFeed().then(data => {
+    //  this.data = data.feed;
         //loadingUI.dismiss();
-    });
+    //});
+  }
+
+  loadTour(evt)
+  {
+    alert('Value Changed!');
+    alert(evt);
+    
+    // let alert = this.alertCtrl.create({
+    //   title: 'Low battery',
+    //   subTitle: '10% of battery remaining',
+    //   buttons: ['Dismiss']
+    // });
+    // alert.present();
   }
 }
